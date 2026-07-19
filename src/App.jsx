937257b7b1498101ea033c95c8a50d314c83ec78
KAVIYARASU08 <skaviyarasuskaviyarasu2008@gmail.com>
2026-07-19 21:48:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,89 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import Anime from "./pages/Anime";
+import Games from "./pages/Games";
+import Engineering from "./pages/Engineering";
+
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+
 import ProtectedRoute from "./components/common/ProtectedRoute";
 
 
-<Route
+function App() {
 
-path="/profile"
 
-element={
+  return (
 
-<ProtectedRoute>
+    <Routes>
 
-<Profile />
 
-</ProtectedRoute>
+      {/* Public Pages */}
+
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+
+      <Route
+        path="/movies"
+        element={<Movies />}
+      />
+
+
+      <Route
+        path="/anime"
+        element={<Anime />}
+      />
+
+
+      <Route
+        path="/games"
+        element={<Games />}
+      />
+
+
+      <Route
+        path="/engineering"
+        element={<Engineering />}
+      />
+
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+
+
+      {/* Protected Pages */}
+
+
+      <Route
+
+        path="/profile"
+
+        element={
+
+          <ProtectedRoute>
+
+            <Profile />
+
+          </ProtectedRoute>
+
+        }
+
+      />
+
+
+    </Routes>
+
+  );
 
 }
 
-/>
+
+export default App;
