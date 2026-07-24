5f63560a6286b2051c506f47c3ee48de70d43614
KAVIYARASU08 <skaviyarasuskaviyarasu2008@gmail.com>
2026-07-24 22:04:42 +0530

    Create Engineering.jsx

---
+++ src/components/engineering/Engineering.jsx
@@ -0,0 +1,20 @@
+import SubjectCard from "../components/engineering/SubjectCard";
+
+
+<SubjectCard
+
+  id="data-structures"
+
+  name="Data Structures"
+
+  code="23CS201"
+
+  semester="III"
+
+  description="Learn arrays, linked lists, stacks, queues, trees and graphs."
+
+  units={5}
+
+  icon="💻"
+
+/>
