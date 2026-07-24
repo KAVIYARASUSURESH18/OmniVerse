4542fc053f3d670eff6a96715f51b1a38b7a706f
KAVIYARASU08 <skaviyarasuskaviyarasu2008@gmail.com>
2026-07-24 22:46:28 +0530

    Create ForgotPasswordForm.jsx

---
+++ src/components/auth/ForgotPasswordForm.jsx
@@ -0,0 +1,14 @@
+import ForgotPasswordForm
+  from "../components/auth/ForgotPassword";
+
+
+function ForgotPassword() {
+
+  return (
+    <ForgotPasswordForm />
+  );
+
+}
+
+
+export default ForgotPassword;
